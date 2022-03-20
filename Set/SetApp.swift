7ce367c7708448e4b757040516042a79b6fb7e9f
amanuelayele <amanuelayele@gmail.com>
2022-03-20 13:11:23 -0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Amanuel Ayele on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
